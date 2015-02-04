a25c96543416041b6a36ce3c28242b283d0fb506
Gabriel Suarez <lixez@live.com.ar>
2015-02-04 16:53:36 -0300

    Notificactions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 // JavaScript Document
 
-
-
 function onBodyLoad() {       
   document.addEventListener("deviceready", onDeviceReady, false);
 }
 
 function onDeviceReady(){
     navigator.notification.alert("VIVIMo 1.0");
-}
+		
+	}
+	
+	

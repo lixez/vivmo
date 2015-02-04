5e736cf94579ae538b6ce2e66179afa03848ee6c
Gabriel Suarez <lixez@live.com.ar>
2015-02-04 19:14:50 -0300

    Nueva version de jquery Mobile!

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 // JavaScript Document
 
-function onBodyLoad() {       
-  document.addEventListener("deviceready", onDeviceReady, false);
-}
+function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    }
 
-function onDeviceReady(){
-    navigator.notification.alert(
-    'You are the winner!',  // message
-    alertDismissed,         // callback
-    'Game Over',            // title
-    'Done'                  // buttonName
-);
-		
-	}
-	
-	
+
+    function onDeviceReady() {
+        
+	navigator.notification.alert("VIVMo 1.0 - Aplicación de Medición de Viveros by Moebius-IT. Le recordamos mantener su aplicación actualizada para mejor funcionamiento.","accion", "Bienvenido a VIVMo", "Continuar");
+}

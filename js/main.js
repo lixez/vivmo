427a61f90cbf3f18fa2339674041ac9fa7b3d7a7
Gabriel Suarez <lixez@live.com.ar>
2015-02-04 11:34:01 -0300

    Activando funcionalidades

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+
+
+function onBodyLoad() {       
+  document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+function onDeviceReady(){
+    navigator.notification.alert("VIVIMo 1.0");
+}

95f6e40a6caf7fd0eebb21cb83ee81c8fc554007
Gabriel Suarez <lixez@live.com.ar>
2015-02-08 17:57:17 -0300

    Nuevas bases de datos

--- js/main.js
+++ js/main.js
@@ -21,7 +21,9 @@ function onDeviceReady() {
 	
 	if(existe_db == null){
 		creaBase();
-}
+}else{
+		cargaDatos();
+	}
 	
 
 
@@ -43,8 +45,15 @@ function creaNuevaBase(tx){
 		
 	tx.executeSql(sql);
 	
-	tx.executeSql("INSERT INTO vivmoDB (id,descripcion) VALUES (1,'descripcionNave')");
-	
+	tx.executeSql("INSERT INTO naves (id,descripcion) VALUES (1,10-A)");
+	tx.executeSql("INSERT INTO naves (id,descripcion) VALUES (2,4-A)");
+	tx.executeSql("INSERT INTO naves (id,descripcion) VALUES (3,5-A)");
+	tx.executeSql("INSERT INTO naves (id,descripcion) VALUES (4,7-A)");
+	tx.executeSql("INSERT INTO naves (id,descripcion) VALUES (5,8-B)");
+	tx.executeSql("INSERT INTO naves (id,descripcion) VALUES (6,9-A)");
+	tx.executeSql("INSERT INTO naves (id,descripcion) VALUES (7,9-B)");
+
+
 	
 	tx.executeSql('DROP TABLE IF EXISTS instalaciones');
 	
@@ -63,35 +72,39 @@ function creaNuevaBase(tx){
 		
 	tx.executeSql(sql);
 	
+	tx.executeSql("INSERT INTO instalaciones (id,instalacion,temporada,nave,codigo,fuente,contenedorparcelas,estratos,especies,unique) VALUES (1,134,2015,10-A,27 B1,T,56,0,4312,TC)");
+
 	
 	tx.executeSql('DROP TABLE IF EXISTS mediciones');
 	
 	var sql = "CREATE TABLE IF NOT EXISTS mediciones ( "+
 		"_id INTEGER PRIMARY KEY AUTOINCREMENT,"+
-		"Fecha DATE, "+
-		"Temporada INTEGER,"+
-		"Instalacion INTEGER, "+
-		"Nave VARCHAR(50),"+ 
-		"Codigo VARCHAR(50),"+
-		"Fuente VARCHAR(10),"+
-		"Contenedor INTEGER, "+
-		"Estratos INTEGER, "+
-		"Especie varchar(10),"+ 
-		"Parcelas INTEGER, "+
-		"Parcela INTEGER,"+
-		"Muertos INTEGER ,"+
-		"Plantas INTEGER,"+
-		"Planta INTEGER, "+
-		"Altura FLOAT,"+
-		"Diametro FLOAT,";
+		"fecha DATE, "+
+		"temporada INTEGER,"+
+		"instalacion INTEGER, "+
+		"nave VARCHAR(50),"+ 
+		"codigo VARCHAR(50),"+
+		"fuente VARCHAR(10),"+
+		"contenedor INTEGER, "+
+		"estratos INTEGER, "+
+		"especie varchar(10),"+ 
+		"parcelas INTEGER, "+
+		"parcela INTEGER,"+
+		"muertos INTEGER ,"+
+		"plantas INTEGER,"+
+		"planta INTEGER, "+
+		"altura FLOAT,"+
+		"diametro FLOAT,";
 		
 	tx.executeSql(sql);
-	
+	tx.executeSql("INSERT INTO mediciones (id,fecha,temporada,instalacion,nave,codigo,fuente,contenedor,estratos,especie,parcelas,parcela,muertos,plantas,planta,altura,diametro) VALUES (1,2014-08-26,2015,79,7-A,27 T,T,88,7392,TC,5,1,28,8,1,4.5,2)");
+
 };
 
 
 function creaBaseSuccess(){
 	window.localStorage.setItem("existe_db", 1);
+	cargaDatos();
 };
 
 
@@ -100,3 +113,31 @@ function errorDB(err){
 	mkLog("Error procesando SQL " + err.code);
 	navigator.notification.alert("Error procesando SQL " + err.code);
 };
+
+
+
+/* 
+* carga de datos desde la base de datos
+*/
+function cargaDatos(){
+	db.transaction(cargaRegistros, errorDB);
+}
+
+function cargaRegistros(tx){
+	mkLog("Cargando registros de la base de datos");
+	tx.executeSql('SELECT * FROM naves', [], cargaDatosSuccess, errorDB);
+}
+
+function cargaDatosSuccess(tx, results){
+	mkLog("Recibidos de la DB " + results.rows.length + " registros");
+	if(results.rows.length == 0){
+		mkLog("No se han recibido registros");
+		navigator.notification.alert("No naves base de datos");
+	}
+	
+	for(var i=0; i<results.rows.length; i++){
+		var navesDatos = results.rows.item(i);
+		var selector = $("#listadoNaves" +" ul");
+		selector.append('<li><a href="#detalleNaves">A-10</a></li>').listview('refresh');
+	}
+}

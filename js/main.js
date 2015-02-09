37489374feb20a9452fce36aff16710ace88959c
Gabriel Suarez <lixez@live.com.ar>
2015-02-09 12:39:31 -0300

    creando y cargando base

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function onDeviceReady() {
 	
 	existe_db = window.localStorage.getItem("existe_db");
 	
-	db = window.openDatabase("agenda_curso", "1.0", "DB de VIVMo", 200000);
+	db = window.openDatabase("naves", "1.0", "DB de VIVMo", 200000);
 	
 	if(existe_db == null){
 		creaDB();
@@ -26,8 +26,7 @@ function onDeviceReady() {
 	}
 	
 };
-		
-	
+
 /* 
 * creación de ña base de datos
 */
@@ -39,22 +38,16 @@ function creaDB(){
 function creaNuevaDB(tx){
 	mkLog("Creando base de datos");
 	
-	tx.executeSql('DROP TABLE IF EXISTS agenda_curso');
+	tx.executeSql('DROP TABLE IF EXISTS naves');
 	
-	var sql = "CREATE TABLE IF NOT EXISTS agenda_curso ( "+
+	var sql = "CREATE TABLE IF NOT EXISTS naves ( "+
 		"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-		"nombre VARCHAR(50), " +
-		"apellidos VARCHAR(50), " +
-		"telefono VARCHAR(30), " +
-		"categoria VARCHAR(30), " +
-		"foto VARCHAR(200), " + 
-		"email VARCHAR(30) )";
+		"nombre VARCHAR(50) )";
 		
 	tx.executeSql(sql);
 	
-	tx.executeSql("INSERT INTO agenda_curso (id,nombre,apellidos,telefono,categoria,foto,email) VALUES (1,'José','Pérez','+34566222666','amigo','','paco@paco.com')");
-	tx.executeSql("INSERT INTO agenda_curso (id,nombre,apellidos,telefono,categoria,foto,email) VALUES (2,'Siro','González','+34555434567','familia','','siro@test.com')");
-	tx.executeSql("INSERT INTO agenda_curso (id,nombre,apellidos,telefono,categoria,foto,email) VALUES (3,'Julio','Rodríguez','+34756222666','trabajo','','julio@test.com')");
+	tx.executeSql("INSERT INTO naves (id,nombre) VALUES (1,'José')");
+	
 	
 }
 
@@ -70,10 +63,6 @@ function errorDB(err){
 }
 
 
-
-
-
-
 /* 
 * carga de datos desde la base de datos
 */
@@ -83,7 +72,7 @@ function cargaDatos(){
 
 function cargaRegistros(tx){
 	mkLog("Cargando registros de la base de datos");
-	tx.executeSql('SELECT * FROM agenda_curso', [], cargaDatosSuccess, errorDB);
+	tx.executeSql('SELECT * FROM naves', [], cargaDatosSuccess, errorDB);
 }
 
 function cargaDatosSuccess(tx, results){
@@ -92,20 +81,5 @@ function cargaDatosSuccess(tx, results){
 		mkLog("No se han recibido registros");
 		navigator.notification.alert("No hay contactos en la base de datos");
 	}
-	
-	for(var i=0; i<results.rows.length; i++){
-		var persona = results.rows.item(i);
-		var selector = $("#lista_" + persona.categoria + " ul");
-		
-		selector.append('<li id="li_'+persona.id+'"><a href="#detalle" data-uid='+persona.id+' class="linkDetalles"><div>'+ persona.nombre +  persona.apellidos + '</div></a><a href="#form"  data-theme="a" data-uid='+persona.id+'  class="linkForm">Predet.</a></li>').listview('refresh');
-	}
-	
-	$(".linkDetalles").click(function(e){
-		$.id = $(this).data("uid");
-	});
-	
-	$(".linkForm").click(function(e){
-		$.id = $(this).data("uid");
-	});
 }
 

368b5ea7604511406a5ea2a81a647e116a99443d
Gabriel Suarez <lixez@live.com.ar>
2015-02-10 10:18:57 -0300

    APK para simulación

--- js/main.js
+++ js/main.js
@@ -15,3 +15,39 @@ function onDeviceReady() {
 	navigator.notification.alert("VIVMo 1.0 - Aplicación de Medición de Viveros by Moebius-IT. Le recordamos mantener su aplicación actualizada para mejor funcionamiento.","accion", "Bienvenido a VIVMo", "Continuar");
 			
 };
+
+
+
+
+
+function alertaPlanta8(){
+	
+	
+	navigator.notification.alert("La carga de datos ha finalizado correctamente.");
+	
+	
+	};
+
+
+
+
+
+
+
+
+
+
+
+ function cargarMedicion()
+    {
+        var str = '';
+        var elem = document.getElementById('medirForm').elements;
+        for(var i = 0; i < elem.length; i++)
+        {
+            str += "<b>Type:</b>" + elem[i].type + "&nbsp&nbsp";
+            str += "<b>Name:</b>" + elem[i].name + "&nbsp;&nbsp;";
+            str += "<b>Value:</b><i>" + elem[i].value + "</i>&nbsp;&nbsp;";
+            str += "<BR>";
+        } 
+        document.getElementById('lblValues').innerHTML = str;
+    }
